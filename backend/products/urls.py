d209518e839fa9d6398ea9aee0d0565e424efccd
atharvarakshak <atharvarakshak23@gmail.com>
2024-02-01 14:17:54 +0530

    1/2/24

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,6 +4,7 @@ from . import views
 # /api/products/
 urlpatterns=[
     path('',views.product_list_create_view) ,
-    path('<int:pk>/',views.product_detail_view) 
+    path('<int:pk>/',views.product_detail_view) ,
+    path('<int:pk>/',views.product_detail_view) ,
     #localhost:8000/api/products/pk/
 ]
