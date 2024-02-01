1f4fbc414fce60528f091d6e35de71295664005d
atharvarakshak <atharvarakshak23@gmail.com>
2024-02-02 00:04:16 +0530

    2/1/24

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics,mixins,permissions,authentication
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.decorators import api_view
@@ -12,6 +12,9 @@ from django.shortcuts import get_object_or_404
 class ProductsListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def perform_create(self,serializer):
         # serializer.save(user = self.request.user)
         print(serializer.validated_data)
@@ -21,7 +24,7 @@ class ProductsListCreateAPIView(generics.ListCreateAPIView):
         if content is None:
             content = title
 
-        serializer.save(content=content)
+        instance = serializer.save(content=content)
         # can send a signal
 product_list_create_view = ProductsListCreateAPIView.as_view()
 
@@ -31,6 +34,29 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     # lookup_field = 'pk'  #each model has one primary key by defualt 
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk' 
+
+    def perform_update(self,serializer):
+        instance = serializer.save() 
+        if not instance.content:
+            instance.content = instance.title
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk' 
+
+    def perform_destroy(self,instance):
+        super().perform_destroy(instance)
+
+product_destroy_view = ProductDestroyAPIView.as_view()
 
 # class ProductListAPIView(generics.RetrieveAPIView):
 #     '''
@@ -48,6 +74,42 @@ product_detail_view = ProductDetailAPIView.as_view()
 #  get and post conditions
 
 # function based view are flexible but confusing
+
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+    ):
+
+    queryset = Product.objects.all()
+    serializer_class= ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self,request,*args,**kwargs):
+        print(args,kwargs)
+        pk=kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request,*args,**kwargs)
+        return self.list(request,*args,**kwargs)
+    
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+        
+    def perform_create(self,serializer):
+        # serializer.save(user = self.request.user)
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        
+        if content is None:
+            content = "this is a single wiew doing cool stuff"
+
+        instance = serializer.save(content=content)
+    
+product_mixin_view = ProductMixinView.as_view()
+
+
 @api_view(['GET','POST'])
 def product_alt_view(request,pk=None,*args,**kwargs):
     method = request.method
--- py_client/create.py
+++ py_client/create.py
@@ -3,6 +3,9 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api/products/"
 
+# http://127.0.0.1:8000
+# session - > psot data
+
 data={"title":"this field is filled",
       "price":34.66}
 get_response = requests.post(endpoint,json=data) # http get req 
