164e3fac1e98f6377de43c048c341233b5ce1ac7
atharvarakshak <atharvarakshak23@gmail.com>
2024-02-03 21:37:53 +0530

    3/2/24

--- py_client/create.py
+++ py_client/create.py
@@ -1,15 +1,18 @@
 
 import requests
 
+headers = {'Authorization': 'Bearer 5e1703633976af2da40b1f525897888c265c73e6'}
 endpoint = "http://127.0.0.1:8000/api/products/"
 
 # http://127.0.0.1:8000
 # session - > psot data
 
 data={"title":"this field is filled",
-      "price":34.66}
-get_response = requests.post(endpoint,json=data) # http get req 
+      "price":34.66
+}
+get_response = requests.post(endpoint,json=data,headers=headers) # http get req 
 
 
 print(get_response.json())
 
+# if token is deleted by the admin it requires to log back in and thus creting a new token
--- py_client/list.py
+++ py_client/list.py
@@ -1,10 +1,19 @@
 
 import requests
 
-endpoint = "http://127.0.0.1:8000/api/products/"
+from getpass import getpass
+auth_endpoint = "http://127.0.0.1:8000/api/auth/"
+username = input("what is your username ?\n")
+password = getpass("What is youe password ?\n")
+auth_response = requests.post(auth_endpoint,json={"username":username,'password':password})
+print(auth_response.json())
 
-
-get_response = requests.get(endpoint) # http get req 
-
-print(get_response.json())
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers ={
+        "Authorization":f"Bearer {token}",
+    }
+    endpoint = "http://127.0.0.1:8000/api/products/"
+    get_response = requests.get(endpoint ,headers=headers) # http get req 
+    print(get_response.json())
 
